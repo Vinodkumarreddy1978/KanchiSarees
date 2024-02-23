4a311f165b4100b3270f9d4e74e0ccb616c874c6
vinod kumar reddy pathinti <21eg112b51@anurag.edu.in>
2024-02-23 21:19:28 +0530

    added products and contacts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
+import Product from "./products";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+     <Product/> 
   );
 }
 
