09d31c5583a727e50a68785825eb2849d40bc014
acr6113 <acrchandan6113@gmail.com>
2024-02-23 13:26:21 +0530

    initialized tailwind css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 function App() {
   return (
-    <div className="App">
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }
 
